0a2ff2e455c47038c9f588cdd3aaea57ae5a016c
Paul-Okello <paulokello273@gmail.com>
2022-02-28 15:56:44 +0300

    ⚡ General Update:  Icon Change

--- frontend/src/components/SocialMedia.js
+++ frontend/src/components/SocialMedia.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { BsTwitter, BsInstagram } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
+import { GrLinkedin, GrGithub } from "react-icons/gr";
 
 const SocialMedia = () => (
   <div className='app__social'>
     <div>
-      <BsTwitter />
+      <a
+        href='https://twitter.com/Lopoz_official'
+        target='_blank'
+        rel='noreferrer'>
+        <BsTwitter />
+      </a>
     </div>
     <div>
-      <FaFacebookF />
+      <a
+        href='https://www.linkedin.com/in/paul-okello-11b3031b0/'
+        target='_blank'
+        rel='noreferrer'>
+        {" "}
+        <GrLinkedin />
+      </a>
     </div>
     <div>
-      <BsInstagram />
+      <a href='https://github.com/Paul-Okello' target='_blank' rel='noreferrer'>
+        <GrGithub />
+      </a>
     </div>
   </div>
 );
